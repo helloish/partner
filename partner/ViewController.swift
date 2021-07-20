0c309cd394eba8bc3cdafb1e523eb76922a72641
Ishwarya Namburu <ishunamburu@gmail.com>
2021-07-20 13:35:14 -0700

    Initial Commit

---
+++ partner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  partner
+//
+//  Created by Srinivasa Namburu on 7/20/21.
+//  Copyright © 2021 kode with klossy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

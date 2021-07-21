3973d27f5a21e0339d2a1bb5b07eff61cc1a0d68
Ishwarya Namburu <ishunamburu@gmail.com>
2021-07-21 10:46:16 -0700

    styled buttons

--- partner/ViewController.swift
+++ partner/ViewController.swift
@@ -11,12 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
     
-    @IBAction func button(_ sender: Any) {
-        label.text = "hello"
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        button1.backgroundColor = UIColor.init(red: 194/255, green: 222/255, blue: 214/255, alpha: 1)
+        button1.layer.cornerRadius = 25.0
+        button1.tintColor = UIColor.white
+        button2.backgroundColor = UIColor.init(red: 237/255, green: 170/255, blue: 146/255, alpha: 1)
+        button2.layer.cornerRadius = 25.0
+        button2.tintColor = UIColor.white
+        button3.backgroundColor = UIColor.init(red: 215/255, green: 178/255, blue: 119/255, alpha: 1)
+        button3.layer.cornerRadius = 25.0
+        button3.tintColor = UIColor.white
         // Do any additional setup after loading the view.
     }
 

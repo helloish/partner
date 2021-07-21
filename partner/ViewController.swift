6065a9e40acfb0e66bc6f0b56a9d7cd1835a93c1
Ishwarya Namburu <ishunamburu@gmail.com>
2021-07-21 12:05:57 -0700

    b

--- partner/ViewController.swift
+++ partner/ViewController.swift
@@ -12,7 +12,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
   
-   
+    
+    @IBAction func button1(_ sender: UIButton) {
+    }
+    
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    
+    @IBAction func button3(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextPage(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()

a659fc41cf857b64e65f754ae6f469e29db86ede
Ishwarya Namburu <ishunamburu@gmail.com>
2021-07-21 21:33:05 -0700

    added 3 gender inequality buttons

---
+++ partner/ViewControllerGE.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerGE.swift
+//  partner
+//
+//  Created by Srinivasa Namburu on 7/21/21.
+//  Copyright © 2021 kode with klossy. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerGE: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = false
+        navigationItem.backBarButtonItem?.title = "back"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
